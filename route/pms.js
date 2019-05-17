ab85c04f674254d9fd40e5e19d7bce62d3e2e873
BOISER <boiserma_sd203a1@CEACCS.NET>
2019-05-17 18:29:15 +0800

    files modified

--- route/pms.js
+++ route/pms.js
@@ -2,6 +2,14 @@ const express = require('express');
 
 const router = express.Router();
 
+router.get('/manager', (req, res) =>{
+    res.render("manager");
+});
+
+router.get('/projects&clients',(req,res) =>{
+    res.render("projects");
+});
+
 router.get('/secSpec',(req,res) =>{
     res.render("secSpec");
 });
